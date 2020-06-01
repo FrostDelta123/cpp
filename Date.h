3da807539f3deb6090e4fea68f840b7a62ea2df1
FrostDelta123 <pawel.soko2012@yandex.ru>
2020-06-01 14:12:00 +0300

    fix final

--- Date.h
+++ Date.h
@@ -5,12 +5,12 @@
 using namespace std;
 class Date {
 public:
-    Date(); //Конструктор класса
-    int day = 0, mon = 0, year = 0; //Стандартные значения
-    static int isValid(int, int, int); // Проверка даты на валидность. День, месяц, год
-    static bool isValid(string);// Проверка даты на валидность. Строка DD.MM.YYYY
-    static bool isValid(Date);// Проверка даты на валидность. Дата.
-    Date(int, int, int); //Конструктор класса, принимающий на вход день, месяц и год
+    Date();
+    int day = 0, mon = 0, year = 0;
+    static int isValid(int, int, int);
+    static bool isValid(string);
+    static bool isValid(Date);
+    Date(int, int, int);
 };
 
 
